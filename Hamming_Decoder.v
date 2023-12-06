c1ee3c485fb37812f29fa5925536152ababab31f
racoonon <95404249+racoonon@users.noreply.github.com>
2023-12-06 17:07:38 +0900

    Update Hamming_Decoder.v

--- Hamming_Decoder.v
+++ Hamming_Decoder.v
@@ -67,3 +67,61 @@ module Hamming_Decoder(
 // ************** //
 
 endmodule
+
+
+
+
+// 수정 후 코드 // 확인바랍니당 
+1) wire은 always 안에서 수정이 안되어서 따로 reg c 선언/ always 문 밖에서 마지막에 data_out으로 assign
+2) err_double_bit 코드
+3) err_single bit 시 수정 코드 
+// **** TODO **** //
+reg A, B, C, D;
+reg err_single_bit, err_double_bit;
+reg [3:0] c;
+
+// Syndrome(A, B, C, D) calculation
+always @(posedge clk or negedge rstn) begin
+        if (~rstn) begin
+            c = {data_in[6], data_in[5], data_in[4], data_in[2]};
+            
+            // Syndrome calculation
+            A = data_in[0] ^ data_in[2] ^ data_in[4] ^ data_in[6];
+            B = data_in[1] ^ data_in[2] ^ data_in[5] ^ data_in[6];
+            C = data_in[1] ^ data_in[4] ^ data_in[5] ^ data_in[6];
+            D = data_in[0] ^ data_in[1] ^ data_in[2] ^ data_in[3] ^ data_in[4] ^ data_in[5] ^ data_in[6] ^ data_in[7];
+
+            // Error number check 
+            err_single_bit = (D == 1'b1);
+            err_double_bit = (!err_single_bit && (A | B | C));
+
+            // Error correctable check
+            if (err_single_bit) begin // only correctable error
+                if (A && B && !C) begin // D1 error: 1 1 0
+                    c[0] = ~c[0];
+                end else if (A && !B && C) begin // D2 error : 1 0 1
+                    c[1] = ~c[1];
+                end else if (!A && B && C) begin // D3 error : 0 1 1
+                    c[2] = ~c[2];
+                end else if (A && B && C) begin // D4 error : 1 1 1 
+                    c[3] = ~c[3];
+                end
+            end
+       end
+ end
+    
+ assign err_uncorrectable = err_double_bit;
+ assign err_correctable = err_single_bit;
+ assign data_out = c;
+// ************** //
+
+endmodule
+
+//
+
+
+
+
+
+
+
