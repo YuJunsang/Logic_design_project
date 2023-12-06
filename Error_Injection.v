04b92db67f30462fd397cabc686fb080639f4dbc
racoonon <95404249+racoonon@users.noreply.github.com>
2023-12-06 16:30:22 +0900

    Update Error_Injection.v

--- Error_Injection.v
+++ Error_Injection.v
@@ -7,9 +7,23 @@ module Error_Injection(
     );
 
 // **** TODO **** //
+reg [7:0] c;
 
-
-
+always @ (posedge clk) begin
+    if (rstn)  c = 8'b00000000; // reset 시?
+    else begin
+        c = data_in;
+        if(btn1 && btn2) begin
+            c[0] = ~c[0];
+            c[4] = ~c[4];
+        end else if (btn1) begin
+            c[0] = ~c[0];
+        end else if (btn2) begin
+            c[4] = ~c[4];
+        end else ;
+    end
+end
+assign data_out = c;
 // ************** // 
 
 endmodule
